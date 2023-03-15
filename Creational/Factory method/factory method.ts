2d5366b800fc0a945910086f5f70ad3dfcdb2109
Voyager <78823465+Voyager101pw@users.noreply.github.com>
2023-03-15 07:00:44 +0500

    Update factory method.ts

--- Creational/Factory method/factory method.ts
+++ Creational/Factory method/factory method.ts
@@ -1,43 +1,22 @@
-interface Product {
-  getName: () => void;
+interface Product {}
+class ConcreteProductA implements Product { /* creation logic */ }
+class ConcreteProductB implements Product { /* creation logic */ }
+
+abstract class Creator {
+  public abstract factoryMethod(): Product;
+  // Factory Method отвечает за создание продуктов, принадлежащих 
+  // одному семейству Product (имеют одинакувую структуру)
+  // но c разной реализацией (разные данные внутри).
 }
 
-class ConcreteProductA implements Product {
-  getName() {
-    console.log('Product A');
-  }
-}
-
-class ConcreteProductB implements Product {
-  getName() {
-    console.log('Product B');
-  }
-}
-
-
-interface Creator {
-  factoryMethod: () => Product;
-  // Фабричный метод - создает множество однотипных объектов
-  // с одинаковой структурой но разной реализацией разными данными
-}
-
-class CreatorProductA implements Creator {
-  factoryMethod() { // Фабричный метод
+class ConcreteCreatorA extends Creator {
+  public factoryMethod(): Product {
     return new ConcreteProductA();
   }
 }
 
-class CreatorProductB implements Creator {
-  factoryMethod() { // Фабричный метод
+class ConcreteCreatorB extends Creator {
+  public factoryMethod(): Product {
     return new ConcreteProductB();
   }
 }
-
-
-const creatorProductA = new CreatorProductA();
-const productA = creatorProductA.factoryMethod();
-productA.getName(); // Product A
-
-const creatorProductB = new CreatorProductB();
-const productB = creatorProductB.factoryMethod();
-productB.getName(); // Product B
